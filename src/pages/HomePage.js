9bcef8a4ac5cf8dc4c1f4ffd70776eb4f48444f6
aisy <aisymuhammad@gmail.com>
2020-10-16 03:53:12 +0700

    add Home page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DefaultLayout from '../layouts/DefaultLayout';
+
+const HomePage = () => {
+  return (
+    <DefaultLayout>
+
+    </DefaultLayout>
+  )
+}
+
+export default HomePage
