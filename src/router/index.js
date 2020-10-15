abcbafff97926655399e2003d0033f86d3a4eb3a
aisy <aisymuhammad@gmail.com>
2020-10-16 03:52:34 +0700

    add router

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+// import page here
+import HomePage from '../pages/HomePage';
+
+const Router = () => {
+  return (
+    <Switch>
+      <Route exact path={"/"} component={HomePage} />
+    </Switch>
+  )
+}
+
+export default Router
