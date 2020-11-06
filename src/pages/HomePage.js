87de493720f64b02db7ce9992c984c499c8ce2a2
aisy <aisymuhammad@gmail.com>
2020-11-07 02:09:58 +0700

    add more component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,15 @@
-import React from 'react';
 import DefaultLayout from '../layouts/DefaultLayout';
+
+import CarouselPromo from '../components/Home/CarouselPromo';
+import ProductCategory from '../components/Home/ProductCategory';
+import FlashSale from '../components/Home/FlashSale';
 
 const HomePage = () => {
   return (
     <DefaultLayout>
-
+      <CarouselPromo />
+      <ProductCategory />
+      <FlashSale />
     </DefaultLayout>
   )
 }
