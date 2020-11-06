22f47d64c2e61e83b60c34bfbe14c1b87fb68e3c
aisy <aisymuhammad@gmail.com>
2020-11-07 02:09:19 +0700

    change style

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -1,13 +1,19 @@
-import React from 'react';
 import { Layout } from 'antd';
 
 import Navbar from '../components/Navbar';
+
+const styles = {
+  containerLayout: {
+    marginTop: 60,
+    // padding: 100,
+  }
+}
 
 const DefaultLayout = ({ children }) => {
   return (
     <Layout>
       <Navbar />
-      <div>
+      <div style={styles.containerLayout}>
         {children}
       </div>
     </Layout>
