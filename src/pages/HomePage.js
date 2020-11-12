df9e297085ca6b977c02d1d52c7bc29fc74c22bf
aisy <aisymuhammad@gmail.com>
2020-11-12 19:14:54 +0700

    add new component in home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,13 +3,20 @@ import DefaultLayout from '../layouts/DefaultLayout';
 import CarouselPromo from '../components/Home/CarouselPromo';
 import ProductCategory from '../components/Home/ProductCategory';
 import FlashSale from '../components/Home/FlashSale';
+import ListCardItems from '../components/ListCardItems';
+import ListCardItemsImage from '../components/ListCardItemsImage';
 
 const HomePage = () => {
   return (
     <DefaultLayout>
       <CarouselPromo />
-      <ProductCategory />
-      <FlashSale />
+
+      <div style={{ paddingLeft: 120, paddingRight: 120 }}>
+        <ProductCategory />
+        <FlashSale />
+        <ListCardItems />
+        <ListCardItemsImage />
+      </div>
     </DefaultLayout>
   )
 }
